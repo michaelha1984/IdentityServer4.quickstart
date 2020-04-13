b75dfcfbf2c2d791398dd892f80c0277ad1c3a00
Michael Ha <michael.ha@outlook.com.au>
2020-04-12 23:17:24 -0700

    Update config with api resource and allowed client.

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -17,11 +17,30 @@ namespace IdentityServer
 
         public static IEnumerable<ApiResource> Apis =>
             new ApiResource[] 
-            { };
+            {
+                new ApiResource("api1", "My API")
+            };
         
         public static IEnumerable<Client> Clients =>
             new Client[] 
-            { };
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
         
     }
 }
