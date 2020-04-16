d6871ffc34aa63d36cd122f6faa316b23728ab15
Michael Ha <michael.ha@outlook.com.au>
2020-04-16 16:50:03 -0700

    Add static files middleware.

--- src/JavascriptClient/Startup.cs
+++ src/JavascriptClient/Startup.cs
@@ -16,6 +16,7 @@ namespace JavascriptClient
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,8 @@ namespace JavascriptClient
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
