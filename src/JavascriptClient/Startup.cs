2200f783248a085a2cb4b5832c1cc3948b2f9e77
Michael Ha <michael.ha@outlook.com.au>
2020-04-16 17:05:16 -0700

    Add ids4 authentication dependencies to startup and cors dependencies and middlewares to allow ajax calls to ids4.

--- src/JavascriptClient/Startup.cs
+++ src/JavascriptClient/Startup.cs
@@ -16,7 +16,27 @@ namespace JavascriptClient
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
 
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ApiName = "api1";
+                });
+
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("default", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5003")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,8 +47,11 @@ namespace JavascriptClient
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors("default");
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
         }
     }
 }
